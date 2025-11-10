40d8a66f1af63b4a642f224eb84a0cefa6a9bf21
Bruna <bruna.righi123@gmail.com>
2025-11-10 10:11:00 -0300

    Mudanças em pino e Sensor

--- sketch_oct15a/Sensor.cpp
+++ sketch_oct15a/Sensor.cpp
@@ -1,20 +1,38 @@
-#include <Arduino.h>
-#include <Pino.h>
+#include "Sensor.h"
 
-class Sensor
-{
-private:
-    Pino[] pins;
-public:
-    Sensor(Pino[] pinos);
-    ~Sensor();
-};
-
-Sensor::Sensor(Pino[] pinos)
-{
-    this->pinos = pinos;
+Sensor::Sensor(Pino* pinos, int numPinos, String name) {
+    this->numPins = numPinos;
+    this->name = name;
+    this->pins = new Pino[numPinos];
+    for(int i = 0; i < numPinos; i++) {
+        this->pins[i] = pinos[i];
+    }
 }
 
-Sensor::~Sensor()
-{
+Sensor::~Sensor() {
+    delete[] pins;
+}
+
+String Sensor::toJSON() {
+    StaticJsonDocument<200> doc;
+    
+    JsonArray pinsArray = doc.createNestedArray("pins");
+    for(size_t i = 0; i < numPins; i++) {
+        JsonObject pinObj = pinsArray.createNestedObject();
+        pinObj["value"] = pins[i].read();
+    }
+    
+    String output;
+    serializeJson(doc, output);
+    return output;
+}
+
+JsonDocument Sensor::readValues() {
+    StaticJsonDocument<200> doc;
+    
+    for(size_t i = 0; i < numPins; i++) {
+        doc[String("pin") + String(pins[i].getPin())] = pins[i].read();
+    }
+    
+    return doc;
 }
---
+++ sketch_oct15a/Sensor.h
@@ -0,0 +1,21 @@
+#ifndef SENSOR_H
+#define SENSOR_H
+
+#include <ArduinJson.h>
+#include <Arduino.h>
+#include <Pino.h>
+
+class Sensor
+{
+private:
+    Pino* pins;
+    int numPins;
+    String name;
+public:
+    Sensor(Pino* pinos, int numPinos, String name);
+    ~Sensor();
+    String toJson();
+    JsonDocument readValues();
+};
+
+#endif
