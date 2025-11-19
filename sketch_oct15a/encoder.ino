6043dad0bb051f94f49567b21974d6f81af53076
Bruna <bruna.righi123@gmail.com>
2025-11-19 09:58:26 -0300

    ESP2 feita

--- sketch_oct15a/encoder.ino
+++ sketch_oct15a/encoder.ino
@@ -1,14 +1,39 @@
 #include "comunicacao.h"
 #define encoder 13
+#define rele 12
 
-bool dado = 0;
+/*
+ *  recebe: 
+ *       destravar = boolean; true se a senha foi correta
+ *  envia:
+ *      ["alerta"] = boolean; true se deu timeout (porta aberta por > 5 segundos)
+ *      ["portal_aberta"] = boolean; true se a porta abriu (está interrompendo o sensor)
+*/
+
+bool dado = LOW;
 long tempo_ini = 0;
 const int intervalo = 5000;
 
+void processarComando(const String& command, const JSONVar& params) {
+  if (command == "destravar") {
+    if (params.hasOwnProperty("destravar")) {
+      bool retornoValue = (bool)params["destravar"];
+      
+      if (retornoValue == true) {
+        digitalWrite(rele, HIGH);
+      }
+    }
+  }
+  else {
+    Serial.println("Comando desconhecido: " + command);
+  }
+}
 
 void setup() {
     pinMode(encoder, INPUT);
-    comunicacaoInit("ESP2_RELE_ENCODER");
+    pinMode(rele, OUTPUT);
+    comunicacaoInit("ESP32_DOOR");
+    comunicacaoSetCallback(processarComando);
     Serial.begin(9600);
 }
 
@@ -17,17 +42,25 @@ void loop() {
     long tempo_atual = millis();
     bool novo_dado = digitalRead(encoder);
     if (tempo_atual - tempo_ini >= intervalo) {
-        
+        if (dado == LOW) {
+            JSONVar dados;
+            dados["alerta"] = true;
+            dados["porta_aberta"] = true;
+            enviarDadosSensor("encoder", dados);
+        }
         tempo_ini = tempo_atual;
     }
     if (novo_dado == HIGH && dado == LOW){
         JSONVar dados;
-        dados["interrompido"] = true;
+        dados["porta_aberta"] = false;
+        dados["alerta"] = false;
         enviarDadosSensor("encoder", dados);
+        digitalWrite(rele, LOW);
         dado = novo_dado;
     } else if (novo_dado == LOW && dado == HIGH) {
         JSONVar dados;
-        dados["interrompido"] = false;
+        dados["porta_aberta"] = true;
+        dados["alerta"] = false;
         enviarDadosSensor("encoder", dados);
         dado = novo_dado;
     }
